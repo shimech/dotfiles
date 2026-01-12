62649b848991de4e9f7df809b0a1df4c1776bf25
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2026-01-12 18:50:35 +0900

    Remove Vim configuration files and associated color scheme from the project.

--- bin/install.sh
+++ bin/install.sh
@@ -10,10 +10,6 @@ ln -sf $WORKDIR/.zlogin $HOME/.zlogin
 ln -sf $WORKDIR/.zlogout $HOME/.zlogout
 ln -sf $WORKDIR/.zpreztorc $HOME/.zpreztorc
 
-# vim
-ln -sf $WORKDIR/.vim $HOME/.vim
-ln -sf $WORKDIR/.vimrc $HOME/.vimrc
-
 # WezTerm
 for f in $WORKDIR/.config/wezterm/*; do
   ln -sf "$f" $HOME/.config/wezterm/
