453ef7fb76910ccdedb04e3a3d179680ecbd47fe
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2026-03-26 10:42:51 +0900

    double

--- .config/wezterm/status.lua
+++ .config/wezterm/status.lua
@@ -1,32 +1,32 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
-local color_fg = '#161821'
+local color_fg = "#161821"
 
 local status_variant = {
   leader = {
-    text = 'LEADER',
-    color_bg = '#b4be82',
+    text = "LEADER",
+    color_bg = "#b4be82",
     icon = wezterm.nerdfonts.oct_sparkle_fill,
   },
   copy = {
-    text = 'COPY',
-    color_bg = '#e2a478',
+    text = "COPY",
+    color_bg = "#e2a478",
     icon = wezterm.nerdfonts.fa_clipboard_list,
   },
   normal = {
-    text = 'NORMAL',
-    color_bg = '#84a0c6',
+    text = "NORMAL",
+    color_bg = "#84a0c6",
   }
 }
 
 local function format_left_status(text, color_bg, icon)
-  local icon_text = icon and ' ' .. icon or ''
+  local icon_text = icon and " " .. icon or ""
 
   return wezterm.format {
-    { Attribute = { Intensity = 'Bold' } },
+    { Attribute = { Intensity = "Bold" } },
     { Background = { Color = color_bg } },
     { Foreground = { Color = color_fg } },
-    { Text = icon_text .. ' ' .. text .. ' ' },
+    { Text = icon_text .. " " .. text .. " " },
     { Background = { Color = color_fg } },
     { Foreground = { Color = color_bg } },
     { Text = wezterm.nerdfonts.pl_left_hard_divider },
@@ -45,15 +45,15 @@ end
 
 local function get_status(window)
   if window:leader_is_active() then
-    return 'leader'
+    return "leader"
   end
 
   local name = window:active_key_table()
-  if name == 'copy_mode' then
-    return 'copy'
+  if name == "copy_mode" then
+    return "copy"
   end
 
-  return 'normal'
+  return "normal"
 end
 
 local function update_left_status(window, pane)
@@ -63,14 +63,14 @@ end
 
 local function update_right_status(window, pane)
   window:set_right_status(wezterm.format({
-    { Background = { Color = '#84a0c6' } },
+    { Background = { Color = "#84a0c6" } },
     { Foreground = { Color = color_fg } },
-    { Text = ' ' .. wezterm.nerdfonts.oct_device_desktop .. ' ' .. pane:get_domain_name() .. ' ' },
+    { Text = " " .. wezterm.nerdfonts.oct_device_desktop .. " " .. pane:get_domain_name() .. " " },
   })
   )
 end
 
-wezterm.on('update-status', function(window, pane)
+wezterm.on("update-status", function(window, pane)
    update_left_status(window, pane)
    update_right_status(window, pane)
   end
--- .config/wezterm/tabname.lua
+++ .config/wezterm/tabname.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 -- 絶対パスを表示用のパスに変換する。
 local function get_display_path(cwd_path, fallback)
@@ -6,21 +6,21 @@ local function get_display_path(cwd_path, fallback)
 
   -- カレントディレクトリがホームディレクトリの場合、"~"で早期リターンする。
   if cwd_path == home then
-    return '~'
+    return "~"
   end
 
   local display_path = cwd_path
   if display_path:sub(1, #home) == home then
     -- カレントディレクトリがホームディレクトリ配下の場合、ホームディレクトリまでを"~"で置換する。
     -- 例: /User/username/Documents/foo/bar/baz -> ~/Documents/foo/bar/baz
-    display_path = '~' .. cwd_path:sub(#home + 1)
+    display_path = "~" .. cwd_path:sub(#home + 1)
   else
     -- カレントディレクトリがホームディレクトリ配下でない場合、フォールバックを返す。
     return fallback
   end
 
   local parts = {}
-  for part in string.gmatch(display_path, '[^/]+') do
+  for part in string.gmatch(display_path, "[^/]+") do
     table.insert(parts, part)
   end
 
@@ -30,11 +30,11 @@ local function get_display_path(cwd_path, fallback)
     parts[i] = parts[i]:sub(1, 1)
   end
 
-  local prefix = (display_path:sub(1, 1) == '/') and '/' or ''
-  return prefix .. table.concat(parts, '/')
+  local prefix = (display_path:sub(1, 1) == "/") and "/" or ""
+  return prefix .. table.concat(parts, "/")
 end
 
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     local pane = tab.active_pane
     local cwd = pane.current_working_dir
 
@@ -43,7 +43,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
       local display_path = get_display_path(cwd.file_path:sub(1, -2), pane.title)
 
       return {
-        { Text = ' ' .. wezterm.nerdfonts.md_tab .. ' ' .. tab.tab_index + 1 .. ' ' .. display_path .. ' ' },
+        { Text = " " .. wezterm.nerdfonts.md_tab .. " " .. tab.tab_index + 1 .. " " .. display_path .. " " },
       }
     end
 
