e4ac4947ca42daf3908d887db0a8b38775a99064
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2026-04-02 21:45:05 +0900

    conform

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  opts = {
+    formatters_by_ft = {
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 3000,
+      lsp_format = "fallback",
+    },
+  },
+}
